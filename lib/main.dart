a45a58d003627e2e16d4fe717b4b07333dae25aa
jishnu6241 <jishnuov2002@gmail.com>
2025-03-06 02:06:09 +0530

    some more changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:college_connect_canteen/features/home/home_screen.dart';
 import 'package:college_connect_canteen/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnaXhtaXhnamhzcHVxYXZxanN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5NDMwMjgsImV4cCI6MjA0ODUxOTAyOH0.gb43EJ1vmms_HHfdvj9bZWmAQ2Fv_qMm_yoSsjHBE8s',
+      url: 'https://sgixmixgjhspuqavqjsu.supabase.co');
   runApp(const MyApp());
 }
 
